c34f5d337679d1c54d221e72a13c20a576f587ec
shmilyou <1159171637@qq.com>
2018-05-23 19:51:42 +0800

    The use of redirect :some search result are not allowed to display|重定向的用法：一些搜索结果是不允许被展现的

--- src/main/java/com/example/demo/controller/TweetController.java
+++ src/main/java/com/example/demo/controller/TweetController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class TweetController {
@@ -16,6 +20,17 @@ public class TweetController {
     @RequestMapping("/")
     public String home(){
         return "searchPage";
+    }
+
+    @RequestMapping(value = "/postSearch",method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes){    //star:RedirectAttributes类用于redirect场景下传送值
+        String search=request.getParameter("search");
+        if(search.toLowerCase().contains("struts")){
+            redirectAttributes.addFlashAttribute("error","Try using spring instead!");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute("search",search);
+        return "redirect:result";
     }
 
     @RequestMapping("/result")
