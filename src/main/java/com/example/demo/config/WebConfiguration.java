b2ecdde012d4e62b3a3972c7ad967e4ed39b6ab2
shmilyou <1159171637@qq.com>
2018-05-26 17:08:44 +0800

    il8n,having some bugs... | 国际化，页面显示有些bug

--- src/main/java/com/example/demo/config/WebConfiguration.java
+++ src/main/java/com/example/demo/config/WebConfiguration.java
@@ -1,8 +1,14 @@
 package com.example.demo.config;
 
 import com.example.demo.dates.Zh_LocalDateFormatter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 import java.time.LocalDate;
 
 //对spring mvc进行自定义配置的类
@@ -11,5 +17,23 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatterForFieldType(LocalDate.class,new Zh_LocalDateFormatter());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new SessionLocaleResolver();
+    }
+
+    //拦截所有发往控制器的请求，并检查lang查询参数,如：http://xxxxx?lang=zh
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor=new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
