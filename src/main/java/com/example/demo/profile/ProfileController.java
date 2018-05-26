bd6c9a5c0585ec85f9b69a347194128197beff57
shmilyou <1159171637@qq.com>
2018-05-26 20:36:06 +0800

    add tastes and bug... | 添加口味列表，bug待定...

--- src/main/java/com/example/demo/profile/ProfileController.java
+++ src/main/java/com/example/demo/profile/ProfileController.java
@@ -2,10 +2,12 @@ package com.example.demo.profile;
 
 import com.example.demo.dates.Zh_LocalDateFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
@@ -22,9 +24,25 @@ public class ProfileController {
         return "profile/profilePage";
     }
 
-    @RequestMapping(value = "/profile",method = RequestMethod.POST)
-    public String saveProfile(@Valid ProfileForm profileForm){
+    @RequestMapping(value = "/profile",params = {"save"} ,method = RequestMethod.POST)
+    public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "profile/profilePage";
+        }
         System.out.println("save ok "+ profileForm);
         return "redirect:/profile";
+    }
+
+    @RequestMapping(value = "/profile",params = {"addTaste"})
+    public String addRow(ProfileForm profileForm){
+        profileForm.getTastes().add(null);
+        return "profile/profilePage";
+    }
+
+    @RequestMapping(value = "/profile",params = {"removeTaste"})
+    public String removeRow(ProfileForm profileForm, HttpServletRequest request){
+        Integer rowId=Integer.valueOf(request.getParameter("removeTaste"));
+        profileForm.getTastes().remove(rowId.intValue());
+        return "profile/profilePage";
     }
 }
